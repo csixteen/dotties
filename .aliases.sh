bccef7134e24cf83bdc72eae97dc98ca422a4cee
Pedro Rodrigues <csixteen@protonmail.com>
2020-11-08 17:08:29 +0000

    Refactoring

---
+++ .aliases.sh
@@ -0,0 +1,68 @@
+#------------
+# Navigation
+#------------
+
+alias ..="cd .."
+alias cd..="cd .."
+
+
+#--------------------------------
+# File manipulation - mv, rm, cp
+#--------------------------------
+
+alias mv='mv -v'
+alias rm='rm -i -v'
+alias cp='cp -v'
+
+
+#--------------------------------
+# Networking and troubleshooting
+#--------------------------------
+
+# `dig` is essentially sending a request for the IP of `myip.opendns.com` to
+# the nameserver `resolver1.opendns.com`. This nameserver automatically sends
+# back the IP address of the originating request, as long as it has received
+# that special request.
+# The `+short` flag instructs `dig` to return a less verbose result.
+alias my_ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias ips="ifconfig | grep -P \"inet\\s\" | awk '{ print \$2 }'"
+
+# For quick troubleshooting. The `-e` flag prints the link-level on each dump line (e.g.
+# MAC layer addresses for Ethernet). The `-vv` adds some extra verbosity (TTL, identification,
+# total length, etc). Finally, the `-XX` prints the data of each packet in Hex and ASCII.
+# Sometimes I use `-tt` to print UNIX timestamps at the beginning of each dump line.
+alias tdump='tcpdump -e -vv -XX'
+
+# Reports CPU statistics and I/O statistics for devices and partitions, every second.
+alias iostat="iostat -xz 1"
+
+# Displays both listening and non-listening TCP sockets, with the process info.
+alias open_conn="ss -tanp"
+
+# Watches for changes on all the open files whose descriptors represent network files. In other words,
+# it continuously watches open connections.
+alias watch_conn="watch -d -n1 lsof -i"
+
+
+#-----------------
+# Gentoo specific
+#-----------------
+
+alias e='emerge --ask --verbose --deep --newuse'
+alias euw='e --update @world'
+
+
+#-------------
+# Extra stuff
+#-------------
+
+# Allows me to run aliases as `sudo`. Essentially, if the last character of an alias is a blank,
+# then the next command word is also checked for alias expansion. I prefer this to using something
+# like alias sudo="sudo ".
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Aliases
+alias sd="sudo "
+
+# Shortcuts
+alias g="git"
+alias v="vim"
+alias diskspace="df -P -kH"
---
+++ .bashrc
@@ -0,0 +1,49 @@
+# Some sanity check
+
+if [[ $- != *i* ]] ; then
+	# Shell is non-interactive.  Be done now!
+	return
+fi
+
+
+#------------------------------------------------------------------
+# Shamelessly copying from Paul Irish's dotfiles.
+# https://github.com/paulirish/dotfiles/blob/master/.bash_profile
+
+for file in ~/.{paths,aliases,shell_prompt}.sh; do
+        [ -r "$file" ] && source "$file"
+done
+unset file
+
+
+# Use standard ISO 8601 timestamp
+# %F equivalent to %Y-%m-%d
+# %T equivalent to %H:%M:%S (24-hours format)
+export HISTTIMEFORMAT='%F %T '
+
+# keep history up to date, across sessions, in realtime
+#  http://unix.stackexchange.com/a/48113
+export HISTCONTROL="ignoredups"                 # no duplicate entries
+export HISTSIZE=100000                          # big big history (default is 500)
+export HISTFILESIZE=$HISTSIZE                   # big big history
+
+# shopt essentially toggles the values of settings. The flag `-s` enables (sets)
+# the `histappend` option. When `histappend` is set, the history list is appended
+# to the file named by the value of the `HISTFILE` env var when the shell exits.
+# `type shopt` returns true if the argument `shopt` is found, and false otherwise.
+type shopt &> /dev/null && shopt -s histappend  # append to history, don't overwrite it
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+
+# Forces Bash to save all lines of a multi-line command in the same
+# history entry.
+shopt -s cmdhist
+
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
