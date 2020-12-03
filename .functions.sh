624f18acaa49e3910989da5fea4a94a3880d42e6
Pedro Rodrigues <csixteen@protonmail.com>
2020-12-03 20:44:07 +0100

    rfc_search function

--- .functions.sh
+++ .functions.sh
@@ -35,3 +35,39 @@ rfc() {
                 curl $rfc_file 2> /dev/null | less
         fi
 }
+
+rfc_search() {
+        local script="
+import re
+import sys
+
+def replace_title(s):
+    g = re.search(r'<a.*>((\w|\s)+)</a>', s)
+    if g:
+        return g.group(1)
+    return s
+
+def replace_desc(s):
+    g = re.search(r'<b>((\w|\s|[#&,!;:\.\/\\\(\)\-_])+)</b>', s)
+    if g:
+        return g.group(1)
+    return s
+
+res = filter(
+    lambda x: x.startswith('<a href') or x.startswith('<b>'),
+    [x.strip() for x in sys.stdin.readlines()]
+)
+res = map(replace_title, res)
+res = map(replace_desc, res)
+
+for i in res:
+    print(i, '-', next(res))
+"
+
+        local query="https://datatracker.ietf.org/doc/search?name=$1&sort=&rfcs=on&by"
+        curl $query 2> /dev/null |     \
+        sed -r '/^\s*$/d' |            \
+        grep -A 5 "td class=\"doc\"" | \
+        python -c "$script"
+}
+
