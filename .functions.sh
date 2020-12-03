2a4800f047db4f90c8265f45318428c746922ecc
Pedro Rodrigues <csixteen@protonmail.com>
2020-12-03 16:40:00 +0100

    rfc function

--- .functions.sh
+++ .functions.sh
@@ -15,3 +15,23 @@ scan_network() {
         IFACE=${1:-wlp1s0}
         sudo iw dev $IFACE scan | grep -E '(^BSS|SSID:)'
 }
+
+# Read an RFC as if it were a man page...sort of.
+# Usage: rfc <RFC number> (e.g. 793)
+rfc() {
+        local rfc_file="https://tools.ietf.org/rfc/rfc${1}.txt"
+        local rfc_exists=$(                                   \
+                curl                                          \
+                --max-time 5                                  \
+                --retry 3                                     \
+                --retry-delay 1                               \
+                --head $rfc_file                              \
+                2> /dev/null | head -n 1 | awk '{ print $2 }'
+        )
+        
+        if [ $rfc_exists -ne 200 ]; then
+                echo "[Error] No such RFC: $1"
+        else
+                curl $rfc_file 2> /dev/null | less
+        fi
+}
