472eca62bc5fd2fe2b94b417983917ec3a28c95c
Pedro Rodrigues <csixteen@protonmail.com>
2022-03-23 08:33:01 +0100

    Interactive Haskell goodies for Emacs

--- .emacs.d/elisp/editor-haskell.el
+++ .emacs.d/elisp/editor-haskell.el
@@ -9,6 +9,32 @@
   :ensure t
   :config
  (setq lsp-haskell-server-path "haskell-language-server-wrapper")
- (setq lsp-haskell-server-args ())
-   ;; Comment/uncomment this line to see interactions between lsp client/server.
-  (setq lsp-log-io t))
+ (setq lsp-haskell-server-args ()))
+ ;; Comment/uncomment this line to see interactions between lsp client/server.
+ ;;(setq lsp-log-io t))
+
+
+;; Interactive Haskell mode settings
+;; Reference: http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html#Interactive-Haskell
+
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
